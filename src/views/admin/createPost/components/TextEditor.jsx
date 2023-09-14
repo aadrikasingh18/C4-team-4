e474ac6633f5732d946894414019caac92efb790
Shanit Paul <shanit.kumarpaul.20@aot.edu.in>
2023-09-14 20:02:29 +0530

    extra

--- src/views/admin/createPost/components/TextEditor.jsx
+++ src/views/admin/createPost/components/TextEditor.jsx
@@ -5,7 +5,9 @@ import 'quill/dist/quill.snow.css';
 
 //const SAVE_INTERVAL_MS = 2000
 const TOOLBAR_OPTIONS = [
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    // [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    //font size
+    [{size: ["small","large","huge",false]}],
     [{ font: [] }],
     [{ list: "ordered" }, { list: "bullet" }],
     ["bold", "italic", "underline", "strike"],
@@ -16,8 +18,6 @@ const TOOLBAR_OPTIONS = [
     [{ script: "sub" }, { script: "super" }],
     //alignment 
     [{ align: [] }],
-    //font size
-    [{size: ["small","large","huge",false]}],
     ["image", "blockquote", "code-block"],
     ["clean"],
 ]
@@ -33,25 +33,71 @@ const TOOLBAR_OPTIONS_1 = [
 ]
 
 const TextEditor = () => {
-    // const { id: documentId } = useParams()
-    // const [socket, setSocket] = useState()
+
     const [quill, setQuill] = useState()
 
+    // Backend code (Socket) with delta integration for real time editing
+
+    // const { id: documentId } = useParams()
+    // const [socket, setSocket] = useState()
     // useEffect(() => {
     //     const s = io("http://localhost:3001")
     //     setSocket(s)
-
+    
     //     return () => {
     //       s.disconnect()
     //     }
     //   }, [])
-    // useEffect(()=>{
-    //     quill.on('text-change', (delta, oldDelta, source)=>{
-    //         if(source !== 'user') return
-
-    //         //
-    //     }) 
-    // })
+    
+    //   useEffect(() => {
+    //     if (socket == null || quill == null) return
+    
+    //     socket.once("load-document", document => {
+    //       quill.setContents(document)
+    //       quill.enable()
+    //     })
+    
+    //     socket.emit("get-document", documentId)
+    //   }, [socket, quill, documentId])
+    
+    //   useEffect(() => {
+    //     if (socket == null || quill == null) return
+    
+    //     const interval = setInterval(() => {
+    //       socket.emit("save-document", quill.getContents())
+    //     }, SAVE_INTERVAL_MS)
+    
+    //     return () => {
+    //       clearInterval(interval)
+    //     }
+    //   }, [socket, quill])
+    
+    //   useEffect(() => {
+    //     if (socket == null || quill == null) return
+    
+    //     const handler = delta => {
+    //       quill.updateContents(delta)
+    //     }
+    //     socket.on("receive-changes", handler)
+    
+    //     return () => {
+    //       socket.off("receive-changes", handler)
+    //     }
+    //   }, [socket, quill])
+    
+    //   useEffect(() => {
+    //     if (socket == null || quill == null) return
+    
+    //     const handler = (delta, oldDelta, source) => {
+    //       if (source !== "user") return
+    //       socket.emit("send-changes", delta)
+    //     }
+    //     quill.on("text-change", handler)
+    
+    //     return () => {
+    //       quill.off("text-change", handler)
+    //     }
+    //   }, [socket, quill])
 
 
     const wrapperRef = useCallback((wrapper) => {
@@ -60,8 +106,8 @@ const TextEditor = () => {
 
         const editor = document.createElement("div");
         wrapper.append(editor);
-        const q = new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS }})
-        const b = new Quill(editor, { theme: "bubble", modules: { toolbar: TOOLBAR_OPTIONS_1 }})
+        const q = new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS }}) //Toolbar on the top
+        const b = new Quill(editor, { theme: "bubble", modules: { toolbar: TOOLBAR_OPTIONS_1 }}) //Tooltip for mini editing
         setQuill(q);
         setQuill(b);
     }, [])
