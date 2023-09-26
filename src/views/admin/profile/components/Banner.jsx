315f4aacf49a0ab69c64b1672c528e167c69815f
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2023-09-26 16:16:20 +0530

    fix : article feed

--- src/views/admin/profile/components/Banner.jsx
+++ src/views/admin/profile/components/Banner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import avatar from "assets/img/avatars/avatar11.png";
 import banner from "assets/img/profile/banner.png";
+
 import Card from "components/card";
 import { useAuth } from "contexts/AuthContext";
 
